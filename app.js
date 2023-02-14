1835323812e0439a1f04b6c478f720b4b480037f
jessermiller <iamjaymiller@yahoo.com>
2023-02-14 01:32:12 -0600

    added some css and html changes, aswell as some  js

--- app.js
+++ app.js
@@ -2,3 +2,27 @@ console.log("Hello World!\n==========\n");
 
 // PROJECT Section
 console.log("PROJECT:\n==========\n");
+
+
+const books = [
+    {
+        title:"Name of The Wind",
+        author:"Patrick Rothfuss",
+        read: true,
+    }
+]
+
+// for adding books
+class book {
+    constructor(title, author, read) {
+        this.title = title;
+        this.author = author;
+        this.read = read;
+    }
+}
+class library {
+    constructor(bookCount, books) {
+
+    }
+}
+
