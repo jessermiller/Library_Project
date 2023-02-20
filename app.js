9254fbcbfeb71a697418aabebc48b4e97d30b2b7
jessermiller <iamjaymiller@yahoo.com>
2023-02-20 01:59:37 -0600

    added some new IDs to html and changed up some JS

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-console.log("Hello World!\n==========\n");
+"use strict"
 
 // PROJECT Section
 console.log("PROJECT:\n==========\n");
+const submitBook = document.getElementById("#addBookButton");
+const unsubmitBook = document.getElementById("#removeBookButton");
+
 
 
 const books = [
@@ -21,8 +24,52 @@ class book {
     }
 }
 class library {
-    constructor(bookCount, books) {
+    constructor(books) {
+        this.bookCount = books.length;
+        this.books = books;
 
     }
+    markRead (checkbox, id) {
+      
+    }
+    
+
+    addBook() {
+        
+       const title = document.getElementById("#bookTitle");
+       const author = document.getElementById("#bookAuthor");
+       const read = document.getElementById("#bookRead");
+       const addedBook = (title.value, author.value, read.checked);
+   
+       const tBody = document.getElementById("tableBody");
+       const tRow = document.createElement("tr")
+       const addTitle = document.createElement("td");
+       const addAuthor = document.createElement("td");
+       const newRead = document.createElement("td");
+       addTitle.textContent = title.value;
+       addAuthor.textContent = author.value;
+       const newCheck = document.createElement("input");
+       newCheck.setAttribute("type", "checkbox");
+       newCheck.checked = read.checked;
+       newCheck.disabled = read.checked;
+       newRead.appendChild(newCheck);
+       tRow.appendChild(addTitle);
+       tRow.appendChild(addAuthor);
+       tRow.appendChild(newRead);
+       tBody.appendChild(tRow);
+
+
+       this.books.push(addedBook);
+       bookCount++;
+
+
+    }
+
+    removeBook () {
+
+    }
+
+    
 }
 
+submitBook.addEventListener("click", addBook);
