1cd351d1cb71c0a1b6604952ce636ff00c0c38f4
jessermiller <iamjaymiller@yahoo.com>
2023-02-27 02:07:55 -0600

    js changes

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ console.log("PROJECT:\n==========\n");
 const submitBook = document.getElementById("addBookButton");
 //const unsubmitBook = document.getElementById("removeBookButton");
 const checkboxMark = document.getElementById("bookRead");
-const removeButtonHTML = document.getElementById("rmvBookButton");
-
+//const removeButtonHTML = document.getElementsByClassName("rmvBookBtnn");
 
 // const books = [{
 //     title: "Name of The Wind",
@@ -15,15 +14,15 @@ const removeButtonHTML = document.getElementById("rmvBookButton");
 // }] 
 
 class Book {
-    constructor(id, title, author, read, btn) {
+    constructor(id, title, author, read) {
         this.id = id;
         this.title = title;
         this.author = author;
         this.read = read;
-        this.btn = btn;
-        
-        
-        
+
+
+
+
     }
 }
 class Library {
@@ -34,7 +33,7 @@ class Library {
             title: 'Name of The Wind',
             author: 'Patrick Rothfuss',
             read: true,
-           btn: null,
+
 
         }];
         this.markRead = this.markRead.bind(this);
@@ -42,7 +41,7 @@ class Library {
 
     }
     markRead(checkbox, id) {
-        
+
         for (
             let book of library.books
         ) {
@@ -53,7 +52,7 @@ class Library {
                 checkbox.disabled = true;
 
 
-            } 
+            }
         }
 
     }
@@ -65,72 +64,90 @@ class Library {
         const title = document.getElementById("bookTitle");
         const author = document.getElementById("bookAuthor");
         const read = document.getElementById("bookRead");
-        const btn = document.getElementById("rmvBookButton");
-        //const removeButton = document.getElementById("rmvBookButton");
+
+
         //const addedBook = (title.value, author.value, read.checked);
         const addedTitle = title.value;
         const addedAuthor = author.value;
         const addedRead = read.value;
-        const addedNewRemoveButt = btn.value;
 
-        let newBook = new Book(this.bookCount, addedTitle, addedAuthor, addedRead, addedNewRemoveButt);
+
+        let newBook = new Book(this.bookCount, addedTitle, addedAuthor, addedRead);
         this.books.push(newBook);
-        
+
         console.log(library.books);
-        
-       
+
+
 
         const tBody = document.getElementById("tableBody");
         const tRow = document.createElement("tr")
+        tRow.setAttribute("id", "tRow");
         const addTitle = document.createElement("td");
         const addAuthor = document.createElement("td");
         const newRead = document.createElement("td");
         const newDelete = document.createElement("td");
         addTitle.textContent = addedTitle;
         addAuthor.textContent = addedAuthor;
-        const newRemoveButt = document.createElement("input");
-        newRemoveButt.setAttribute = ("type", "button");
-        newRemoveButt.id = 1;
-        newRemoveButt.innerText = "delete";
-        newDelete.appendChild(newRemoveButt);
+        const newRemoveButt = document.createElement("button");
+
+        newRemoveButt.setAttribute("id", "removeBookBtn");
+        newRemoveButt.setAttribute("class", "removeBookBtnn");
+
+        newRemoveButt.innerText = "Delete";
+        
+
+        
         const newCheck = document.createElement("input");
         newCheck.setAttribute("type", "checkbox");
         newCheck.checked = newBook.read;
         newCheck.disabled = newBook.read;
-        checkboxMark.addEventListener("click", (addBook)=> {
+        checkboxMark.addEventListener("click", (addBook) => {
             library.markRead(newCheck);
         });
-        // add event listener on checkbox that runs a function called library.markedread(newCheck)
         newDelete.appendChild(newRemoveButt);
         newRead.appendChild(newCheck);
         tRow.appendChild(addTitle);
         tRow.appendChild(addAuthor);
         tRow.appendChild(newRead);
+        tRow.appendChild(newDelete);
         tBody.appendChild(tRow);
-       
+
 
 
 
         this.bookCount++;
+        
 
 
     }
+   
 
+    removeItem() {
+        console.log("runnin!");
 
-    //   removeBook()  {
         
-    //     for (books of library.books) {
-    //          if (id == );
-    //       }
-          
-    //   }
+           if (this.book != "") {
+            tRow.remove();
+            
+           }
+           
+    }
 
 
 }
-let library = new Library();
 
+let library = new Library();
 
 
 submitBook.addEventListener("click", () => {
     library.addBook(library.books, library.bookCount);
+    const removeButtonHTML = document.getElementsById("removeBookBtn");
+
+    
 });
+
+
+removeButtonHTML.addEventListener("click", () => {
+    library.removeItem(library.books, library.bookCount);
+})
+
